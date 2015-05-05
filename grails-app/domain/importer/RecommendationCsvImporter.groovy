7619949e5fcfd84690434ac566d85e055225daed
mateodurante <mateodurante@covani>
2015-05-05 11:10:21 -0300

    validacion de carga importer

--- grails-app/domain/importer/RecommendationCsvImporter.groovy
+++ grails-app/domain/importer/RecommendationCsvImporter.groovy
@@ -14,38 +14,65 @@ class RecommendationCsvImporter {
 	static	constraints = {
     }
 	
-	
-	def importFile(){
-		String rows = new File("web-app/last_import.csv").getText('UTF-8')
+	def importFile(String splitChar) {
+		String rows = new File('web-app/last_import.csv').getText('UTF-8')
+		def regExp = splitChar+'(?=([^\"]*\"[^\"]*\")*[^\"]*$)';
 		//String rows = file.getText('UTF-8')
 		//println(rows)
 		def lines = rows.split('\n')
-//		def x = lines*.split(';')
 		
-		def x = lines*.split(';(?=([^\"]*\"[^\"]*\")*[^\"]*$)')
-		//println x
-//		println "valores en 0: "+x*.getAt(0)
-		lines.each { line -> 
-			//def cells = line.split(';')
-			def cells = line.split(';(?=([^\"]*\"[^\"]*\")*[^\"]*$)')
-			println(cells)
-			def project = Project.findByName(cells[0])
-			if (project) {
-				def params = [
-					project:project.id,
-					property:cells[1],
-					path:cells[2],
-					fromPage:cells[3],
-					toPage:cells[4],
-					instructions:cells[5],
-					checkboxMode:cells[6]
-					]
-				Recommendation recomm = new Recommendation(params)
-				recomm.save(flush:true)
-			} else {
-			// TODO EL project no existe
+		def splittedLines = lines*.split(regExp)
+		def projectNames = splittedLines*.getAt(0)
+		def checkboxModes = splittedLines*.getAt(6)
+	
+//		if (!Recommendation.constraints.checkboxMode.getAppliedConstraint('inList').(checkboxModes)){
+//			println "un checkbox falla"
+//		}
+		if (!projectNames.contains('')) {
+			def projects = []
+			projectNames.each { p ->
+				projects.add(Project.findByName(p))
 			}
+			if (!projects.contains(null)) {
+				splittedLines.each { cells -> 
+					def project = Project.findByName(cells[0])
+					if (project) {
+						def params = [
+							project:project.id,
+							property:cells[1],
+							path:cells[2],
+							fromPage:cells[3],
+							toPage:cells[4],
+							instructions:cells[5],
+							checkboxMode:cells[6]
+							]
+						Recommendation recomm = new Recommendation(params)
+						if (recomm.validate()) {
+							recomm.save(flush:true)
+						} else {
+							recomm.errors.allErrors.each {
+								println it
+							}
+						}
+					} else {
+					
+					}
+				}
+			} else {
+				// TODO hay project inexistente
+				println "Project inexistente"
+				return new Error("Some project name does not exists.")
+			}
+		} else {
+			// TODO El project es vacio
+			println "Project vacio"
+			return new Error("Field 'project' is empty on some file line.")
 		}
+		
+	}
+	
+	def importFile() {
+		this.importFile(';')
 	}
 	
 }
--- test/unit/importer/RecommendationCsvImporterSpec.groovy
+++ test/unit/importer/RecommendationCsvImporterSpec.groovy
@@ -15,16 +15,18 @@ import bfcrowd.*
 class RecommendationCsvImporterSpec extends Specification {
 
     def setup() {
+		def project1 = new Project(name: "Project1", description: "Help us improve the contents on Wikipedia!", xpValue: 25)
+		def project2 = new Project(name: "Project2", description: "Help us improve the contents on Wikipedia!", xpValue: 25)
 		def project3 = new Project(name: "Project3", description: "Help us improve the contents on Wikipedia!", xpValue: 25)
 //		project1.save()
-		mockDomain(Project, [project3])
+		mockDomain(Project, [project1,project2,project3])
 		mockDomain(Recommendation)
     }
 
     def cleanup() {
     }
 
-    void "test importFile"() {
+    void "test importFile with a normal file"() {
 		when:
 		def importer = new RecommendationCsvImporter()
 		BufferedWriter writer = null;
@@ -33,16 +35,15 @@ class RecommendationCsvImporterSpec extends Specification {
 			File logFile = new File("web-app/last_import.csv");
 			
 			writer = new BufferedWriter(new FileWriter(logFile));
-			writer.write('''1project;property;path;fromPage;toPage;Instrucctions;Checkbox
+			writer.write('''Project1;property;path;fromPage;toPage;Instrucctions;Checkbox
 Project1;fff;asdf;fdsa;fdf;fff;Checkbox
 Project1;fff2;asdf2 fds fdsf;fdsa2;fdf2;fff2;Checkbox
 Project1;fff2;asdf2 fds fdsf;fdsa2;fdf2;fff2;Checkbox
-;LLLL;LLLL;LLLL;LLLL;LLLL;Checkbox
-Project2;A1;A1;A1;A1;A1;Checkboxdfs
+Project2;LLLL;LLLL;LLLL;LLLL;LLLL;Checkbox
+Project2;A1;A1;A1;A1;A1;Checkbox
 Project2;fff2;asdf2 fds fdsf;fdsa2;fdf2;fff2;Checkbox
-;CCC;CCC;CCC;CCC;CCC;Checkbox
-Project2;GGGG;GGGG;GGGG;GGGG;GGGG;Checkbox
-Project3;propertyAA;pathBB;no?;asdf;asdf;Checkbox''');
+Project3;CCC;CCC;CCC;CCC;CCC;Checkbox
+Project3;GGGG;GGGG;GGGG;GGGG;GGGG;Checkbox''');
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -55,7 +56,9 @@ Project3;propertyAA;pathBB;no?;asdf;asdf;Checkbox''');
 		importer.importFile()
 		
 		then:
-		println Project.getAll()
-		Project.findByName("Project3").recommendations.size() == 1
+//		println Project.getAll()
+		Project.findByName("Project1").recommendations.size() == 4
+		Project.findByName("Project2").recommendations.size() == 3
+		Project.findByName("Project3").recommendations.size() == 2
     }
 }
